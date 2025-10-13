15c20b03db218fd86cd5d4bca0b83b353e8e43d7
Cafe-GameDev <soarxz.games@gmail.com>
2025-10-13 19:48:18 -0300

    Update editor_plugin.gd

--- scripts/editor_plugin.gd
+++ scripts/editor_plugin.gd
@@ -3,9 +3,9 @@ extends EditorPlugin
 
 const AUTOLOAD_NAME = "CoreEngine"
 const AUTOLOAD_PATH = "res://addons/core_engine/utils/core_engine.gd"
+const PANEL_SCENE_PATH = "res://addons/core_engine/panel/core_panel.tscn"
 
 var plugin_panel: ScrollContainer
-var group_panel: VBoxContainer
 
 func _enter_tree():
 	if not ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
@@ -28,21 +28,10 @@ func _create_plugin_panel():
 	if plugin_panel:
 		return
 
-	plugin_panel = ScrollContainer.new()
-	plugin_panel.name = "CorePanel"
-	plugin_panel.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	plugin_panel.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-	plugin_panel.horizontal_scroll_mode = ScrollContainer.SCROLL_MODE_DISABLED
-	plugin_panel.set_follow_focus(true)
-
-	var vbox_container = VBoxContainer.new()
-	vbox_container.name = "VBoxContainer"
-	vbox_container.set_v_size_flags(Control.SIZE_EXPAND_FILL)
-	vbox_container.set_h_size_flags(Control.SIZE_EXPAND_FILL)
-	vbox_container.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-	vbox_container.add_theme_constant_override("separation", 12)
-	
-	plugin_panel.add_child(vbox_container)
-
-	add_control_to_dock(DOCK_SLOT_RIGHT_UL, plugin_panel)
-	CoreEngine.CorePanel = true
+	var scene = load(PANEL_SCENE_PATH)
+	if scene and scene is PackedScene:
+		plugin_panel = scene.instantiate()
+		add_control_to_dock(DOCK_SLOT_RIGHT_UL, plugin_panel)
+		CoreEngine.CorePanel = true
+	else:
+		push_error("Could not load CorePanel scene from: " + PANEL_SCENE_PATH)
