029e0799a9d41da0115ad51a1a79097af8209332
Cafe-GameDev <soarxz.games@gmail.com>
2025-10-13 17:42:11 -0300

    Update editor_plugin.gd

--- scripts/editor_plugin.gd
+++ scripts/editor_plugin.gd
@@ -2,8 +2,8 @@
 extends EditorPlugin
 
 const AUTOLOAD_NAME = "CoreEngine"
-const AUTOLOAD_PATH = "res://addons/data_behavior/utils/core_engine.gd"
-const GROUP_SCENE_PATH = "res://addons/data_behavior/panel/data_panel.tscn"
+const AUTOLOAD_PATH = "res://addons/core_engine/utils/core_engine.gd"
+const GROUP_SCENE_PATH = "res://addons/core_engine/panel/data_panel.tscn"
 
 var plugin_panel: ScrollContainer
 var group_panel: VBoxContainer
@@ -13,14 +13,10 @@ func _enter_tree():
 		add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 	
 	_create_plugin_panel()
-	_register_custom_types()
 
 func _exit_tree():
 	if ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
 		remove_autoload_singleton(AUTOLOAD_NAME)
-	
-	if is_instance_valid(group_panel):
-		group_panel.free()
 
 	if is_instance_valid(plugin_panel):
 		var content_container = plugin_panel.get_node_or_null("VBoxContainer")
@@ -28,13 +24,10 @@ func _exit_tree():
 			if plugin_panel.get_parent() != null:
 				remove_control_from_docks(plugin_panel)
 			plugin_panel.free()
-	
-	_unregister_custom_types()
 
 func _create_plugin_panel():
 	plugin_panel = get_editor_interface().get_base_control().find_child("CafeEngine", true, false)
 	if plugin_panel:
-		_ensure_group("DataBehavior")
 		return
 
 	plugin_panel = ScrollContainer.new()
@@ -54,56 +47,3 @@ func _create_plugin_panel():
 	plugin_panel.add_child(vbox_container)
 
 	add_control_to_dock(DOCK_SLOT_RIGHT_UL, plugin_panel)
-	_ensure_group("DataBehavior")
-
-func _ensure_group(group_name: String) -> VBoxContainer:
-	if not plugin_panel:
-		push_error("Main panel 'CafeEngine' reference not found.")
-		return null
-
-	var content_container = plugin_panel.get_node_or_null("VBoxContainer")
-	if not content_container:
-		push_error("The 'CafeEngine' panel does not contain the expected 'VBoxContainer'.")
-		return null
-
-	group_panel = content_container.find_child(group_name, false)
-	if group_panel:
-		const SCAFFOLDING_CONFIG_PATH = "res://addons/data_behavior/resources/data_config.tres"
-		var data_config_res = ResourceLoader.load(SCAFFOLDING_CONFIG_PATH)
-		if not data_config_res:
-			data_config_res = preload("res://addons/data_behavior/scripts/data_config.gd").new()
-			var dir = SCAFFOLDING_CONFIG_PATH.get_base_dir()
-			if not DirAccess.dir_exists_absolute(ProjectSettings.globalize_path(dir)):
-				DirAccess.make_dir_recursive_absolute(ProjectSettings.globalize_path(dir))
-			
-			var error = ResourceSaver.save(data_config_res, SCAFFOLDING_CONFIG_PATH)
-			if error != OK:
-				push_error("Failed to create and save a new DataConfig resource: %s" % error)
-		group_panel.set_data_config(data_config_res)
-		return group_panel
-
-	var group_scene = load(GROUP_SCENE_PATH)
-	if group_scene and group_scene is PackedScene:
-		group_panel = group_scene.instantiate()
-		content_container.add_child(group_panel)
-		group_panel.name = group_name
-
-		const SCAFFOLDING_CONFIG_PATH = "res://addons/data_behavior/resources/data_config.tres"
-		var data_config_res = ResourceLoader.load(SCAFFOLDING_CONFIG_PATH)
-
-		if not data_config_res:
-			data_config_res = preload("res://addons/data_behavior/scripts/data_config.gd").new()
-			var dir = SCAFFOLDING_CONFIG_PATH.get_base_dir()
-			if not DirAccess.dir_exists_absolute(ProjectSettings.globalize_path(dir)):
-				DirAccess.make_dir_recursive_absolute(ProjectSettings.globalize_path(dir))
-			
-			var error = ResourceSaver.save(data_config_res, SCAFFOLDING_CONFIG_PATH)
-			if error != OK:
-				push_error("Failed to create and save a new DataConfig resource: %s" % error)
-		
-		group_panel.set_data_config(data_config_res)
-
-		return group_panel
-	
-	push_error("Could not load group scene: " + group_name)
-	return null
