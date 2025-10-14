d1bd8586c57e1374ac8c5c6a574eea6b59e88784
Cafe-GameDev <soarxz.games@gmail.com>
2025-10-14 17:58:36 -0300

    s

--- scripts/editor_plugin.gd
+++ scripts/editor_plugin.gd
@@ -3,15 +3,26 @@ extends EditorPlugin
 
 const AUTOLOAD_NAME = "CoreEngine"
 const AUTOLOAD_PATH = "res://addons/core_engine/utils/core_engine.gd"
-const PANEL_SCENE_PATH = "res://addons/core_engine/panel/core_panel.tscn"
+const PANEL_SCENE_PATH = "res://addons/core_engine/panel/core_side_panel.tscn"
+const RESOURCE_TOP_PANEL_SCENE_PATH = "res://addons/core_engine/panel/resource_top_panel.tscn"
+const BLUEPRINT_TOP_PANEL_SCENE_PATH = "res://addons/core_engine/panel/blueprint_top_panel.tscn"
 
 var plugin_panel: ScrollContainer
+var resource_top_panel_instance: Control
 
 func _enter_tree():
 	if not ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
 		add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 	
 	_create_plugin_panel()
+
+	var scene = load(RESOURCE_TOP_PANEL_SCENE_PATH)
+	if scene and scene is PackedScene:
+		resource_top_panel_instance = scene.instantiate()
+		EditorInterface.get_editor_main_screen().add_child(resource_top_panel_instance)
+		resource_top_panel_instance.hide()
+	else:
+		push_error("Could not load CoreTopPanel scene from: " + RESOURCE_TOP_PANEL_SCENE_PATH)
 
 func _exit_tree():
 	if ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
@@ -21,6 +32,22 @@ func _exit_tree():
 		if plugin_panel.get_parent() != null:
 			remove_control_from_docks(plugin_panel)
 		plugin_panel.free()
+
+	if is_instance_valid(resource_top_panel_instance):
+		resource_top_panel_instance.queue_free()
+
+func _create_top_panel():
+	pass
+
+func _has_main_screen():
+	return true
+
+func _make_visible(visible):
+	if is_instance_valid(resource_top_panel_instance):
+		resource_top_panel_instance.visible = visible
+
+func _get_plugin_name():
+	return "Resources"
 
 func _create_plugin_panel():
 	plugin_panel = get_editor_interface().get_base_control().find_child("CorePanel", true, false)
