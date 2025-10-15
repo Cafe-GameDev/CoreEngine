b8d8e02a3632bd6ea39ed57d279161c65d422da6
Cafe-GameDev <soarxz.games@gmail.com>
2025-10-14 21:31:39 -0300

    Update editor_plugin.gd

--- scripts/editor_plugin.gd
+++ scripts/editor_plugin.gd
@@ -9,10 +9,16 @@ const RESOURCE_TOP_PANEL_SCENE_PATH = "res://addons/core_engine/panel/resource_t
 var plugin_panel: ScrollContainer
 var resource_top_panel_instance: Control
 
-func _enter_tree():
+func _enable_plugin() -> void:
 	if not ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
 		add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
-	
+
+
+func _disable_plugin() -> void:
+	if ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
+		remove_autoload_singleton(AUTOLOAD_NAME)
+
+func _enter_tree():	
 	_create_plugin_panel()
 
 	var scene = load(RESOURCE_TOP_PANEL_SCENE_PATH)
@@ -24,9 +30,6 @@ func _enter_tree():
 		push_error("Could not load ResourceTopPanel scene from: " + RESOURCE_TOP_PANEL_SCENE_PATH)
 
 func _exit_tree():
-	if ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
-		remove_autoload_singleton(AUTOLOAD_NAME)
-
 	if is_instance_valid(plugin_panel):
 		if plugin_panel.get_parent() != null:
 			remove_control_from_docks(plugin_panel)
