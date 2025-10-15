64f72adb9e4177ff7e3be73fe04b28ffec0ce5d4
Cafe-GameDev <soarxz.games@gmail.com>
2025-10-14 22:25:49 -0300

    Update editor_plugin.gd

--- scripts/editor_plugin.gd
+++ scripts/editor_plugin.gd
@@ -13,7 +13,6 @@ func _enable_plugin() -> void:
 	if not ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
 		add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 
-
 func _disable_plugin() -> void:
 	if ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
 		remove_autoload_singleton(AUTOLOAD_NAME)
